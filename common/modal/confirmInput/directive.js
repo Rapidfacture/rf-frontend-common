cf96256061518b128ff689633d147b82f302e512
Felix Furtmayr <ff@rapidfacture.com>
2019-12-09 17:54:48 +0100

    update confirm input modal

--- common/modal/confirmInput/directive.js
+++ common/modal/confirmInput/directive.js
@@ -1,4 +1,19 @@
-app.directive('rfModalConfirmInput', [function () {
+/** modal with confirm input
+ *
+ * @version 0.0.5
+ * $scope.$emit('modal', 'confirm-input', 'removeFile', {
+ *   warningText: 'thisRequiresRestartingWhichWillTerminateRunningTransactionsContinue',
+ *   confirmText: 'restart',
+ *   onSuccess: function () {
+ *      $scope.save(function () {
+ *
+ *       });
+ *    },
+ *    onFailure: function () {}
+ * });
+*/
+
+app.directive('rfModalConfirmInput', ['langfactory', function (langfactory) {
    return {
       restrict: 'E',
       templateUrl: 'global/common/modal/confirmInput/template.html',
@@ -6,6 +21,7 @@ app.directive('rfModalConfirmInput', [function () {
       link: function ($scope, elem, attr, ctrl) {
          $scope.modal.onSuccess = $scope.modal.onSuccess || function () {};
          $scope.confirmText = $scope.modal.confirmText || 'CONFIRM';
+         $scope.warningText = langfactory.translate($scope.modal.warningText || 'cantBeUndone');
       }
    };
 }]);
