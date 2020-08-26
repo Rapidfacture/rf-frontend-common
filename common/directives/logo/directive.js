6cf012f63d9b28b2f9460d9a0c3a1a9285daabf4
Alexander Kohl <ak@rapidfacture.com>
2020-08-26 14:31:24 +0200

    bugfix logos

--- common/directives/logo/directive.js
+++ common/directives/logo/directive.js
@@ -23,39 +23,55 @@ app.directive('rfLogo', ['loginFactory', 'http', function (loginFactory, http) {
       },
       link: function ($scope, elem, attr) {
 
+         $scope.logos = {};
          var appName = loginFactory.getAppName() || 'rf-app-cad';
 
          var urls = loginFactory.getAppUrls(appName);
          if (urls.main) $scope.mainUrl = urls.main;
 
-         var files = {};
 
-         if (loginFactory.getAppLogos(appName)) {
-            files = loginFactory.getAppLogos(appName) || {};
-         } else {
-            http.post('get-app-logos', function (files) {
-               files = files || {};
-            });
+
+         function getLogos (callback) {
+            var files = {};
+
+            if (loginFactory.getAppLogos(appName)) {
+               files = loginFactory.getAppLogos(appName) || {};
+               callback(files);
+            } else {
+               http.post('get-app-logos', function (files) {
+                  files = files || {};
+                  callback(files);
+               });
+            }
          }
 
-         var logos = {};
+         function setLogos (files) {
+            var logos = {};
+            console.log($scope.logos);
+            if (files.lg && files.lg.fileId && files.lg.extension) logos.lg = '/static/public-files/' + files.lg.fileId + '.' + files.lg.extension;
+            if (files.md && files.md.fileId && files.md.extension) logos.md = '/static/public-files/' + files.md.fileId + '.' + files.md.extension;
+            if (files.sm && files.sm.fileId && files.sm.extension) logos.sm = '/static/public-files/' + files.sm.fileId + '.' + files.sm.extension;
+            if (files.xs && files.xs.fileId && files.xs.extension) logos.xs = '/static/public-files/' + files.xs.fileId + '.' + files.xs.extension;
 
-         if (files.lg && files.lg.fileId && files.lg.extension) logos.lg = '/static/public-files/' + files.lg.fileId + '.' + files.lg.extension;
-         if (files.md && files.md.fileId && files.md.extension) logos.md = '/static/public-files/' + files.md.fileId + '.' + files.md.extension;
-         if (files.sm && files.sm.fileId && files.sm.extension) logos.sm = '/static/public-files/' + files.sm.fileId + '.' + files.sm.extension;
-         if (files.xs && files.xs.fileId && files.xs.extension) logos.xs = '/static/public-files/' + files.xs.fileId + '.' + files.xs.extension;
+            if (!logos.xs) var xs = logos.xs || logos.sm || logos.md || logos.lg || '/common/img/icon-lg.svg';
+            if (!logos.sm) var sm = logos.sm || logos.md || logos.lg || logos.xs || '/common/img/logo-xs.svg';
+            if (!logos.md) var md = logos.lg || logos.sm || logos.xs || '/common/img/logo-sm.svg';
+            if (!logos.lg) var lg = logos.md || logos.sm || logos.xs || '/common/img/logo-lg.svg';
 
-         if (!logos.xs) var xs = logos.xs || logos.sm || logos.md || logos.lg || '/common/img/icon-lg.svg';
-         if (!logos.sm) var sm = logos.sm || logos.md || logos.lg || logos.xs || '/common/img/logo-xs.svg';
-         if (!logos.md) var md = logos.lg || logos.sm || logos.xs || '/common/img/logo-sm.svg';
-         if (!logos.lg) var lg = logos.md || logos.sm || logos.xs || '/common/img/logo-lg.svg';
+            logos.xs = logos.xs || xs;
+            logos.sm = logos.sm || sm;
+            logos.md = logos.md || md;
+            logos.lg = logos.lg || lg;
 
-         logos.xs = logos.xs || xs;
-         logos.sm = logos.sm || sm;
-         logos.md = logos.md || md;
-         logos.lg = logos.lg || lg;
+            $scope.logos = logos;
+         }
 
-         $scope.logos = logos;
+
+
+         getLogos(function (files) {
+            setLogos(files);
+         });
+
       }
    };
 }]);
